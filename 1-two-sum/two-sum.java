4a7fd8d7289be1c99688194c2ab053c5f196b121
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-10-15 17:12:56 +0530

    Time: 2 ms (98.98%) | Memory: 44.7 MB (95.91%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Solution {
+    public int[] twoSum(int[] arr, int tgt) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int idx = 0; idx < arr.length; idx++) {
+            int req = tgt - arr[idx];
+            if (map.containsKey(req)) {
+                return new int[] { map.get(req), idx };
+            }
+            map.put(arr[idx], idx);
+        }
+        throw new IllegalArgumentException("No two sum solution found.");
+    }
+}
