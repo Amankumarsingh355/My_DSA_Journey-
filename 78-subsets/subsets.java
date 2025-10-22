ba4019f70dc4ed42a9d26de54e0dae9ff829b00b
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-10-22 22:23:37 +0530

    Time: 1 ms (47.93%) | Memory: 42.8 MB (67.19%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+    private void backtrack(int[] nums, int idx, List<Integer> cur, List<List<Integer>> res) {
+        res.add(new ArrayList<>(cur));
+        for (int i = idx; i < nums.length; i++) {
+            cur.add(nums[i]);
+            backtrack(nums, i + 1, cur, res);
+            cur.remove(cur.size() - 1);
+        }
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.subsets(new int[]{1,2,3}));
+    }
+}
