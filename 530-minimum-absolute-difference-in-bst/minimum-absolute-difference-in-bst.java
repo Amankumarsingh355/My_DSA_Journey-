d018f53cac6424d4397c23f6c993e660811fe6cd
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-10-28 03:50:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (33.26%) - LeetSync

---
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.java
@@ -0,0 +1,19 @@
+class Solution {
+    private Integer prev = null;
+    private int best = Integer.MAX_VALUE;
+
+    public int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return best;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+        inorder(node.left);
+        if (prev != null) {
+            best = Math.min(best, node.val - prev);
+        }
+        prev = node.val;
+        inorder(node.right);
+    }
+}
