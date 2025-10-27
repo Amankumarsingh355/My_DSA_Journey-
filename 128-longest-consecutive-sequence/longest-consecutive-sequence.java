bea40765d0e9dade55b1f3449ffc3524e2889c5c
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-10-28 03:58:12 +0530

    Time: 35 ms (24.17%) | Memory: 67.5 MB (51.09%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -21,6 +21,6 @@ public class Solution {
         Solution s = new Solution();
         System.out.println(s.longestConsecutive(new int[]{100,4,200,1,3,2}));
         System.out.println(s.longestConsecutive(new int[]{0,3,7,2,5,8,4,6,0,1}));
-        System.out.println(s.longestConsecutive(new int[]{1,0,1,2})); // 3
+        System.out.println(s.longestConsecutive(new int[]{1,0,1,2}));
     }
 }
