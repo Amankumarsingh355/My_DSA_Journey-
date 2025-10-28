9cbcb64ddd87e0bd3b096470b92ba13a2920dc14
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-10-29 00:44:19 +0530

    Time: 18 ms (55.19%) | Memory: 44.9 MB (72.81%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,32 +1,39 @@
-public class Solution {
+class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() == 0 || t.length() == 0) return "";
-        Map<Character, Integer> targetFreq = new HashMap<>();
-        for (char c : t.toCharArray())
-            targetFreq.put(c, targetFreq.getOrDefault(c, 0) + 1);
-        Map<Character, Integer> windowFreq = new HashMap<>();
-        int required = targetFreq.size();
-        int formed = 0;
-        int left = 0, right = 0;
-        int minLen = Integer.MAX_VALUE, minStart = 0;
-        while (right < s.length()) {
-            char c = s.charAt(right);
-            windowFreq.put(c, windowFreq.getOrDefault(c, 0) + 1);
-            if (targetFreq.containsKey(c) && windowFreq.get(c).intValue() == targetFreq.get(c).intValue())
-                formed++;
-            while (left <= right && formed == required) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                    minStart = left;
-                }
-                char d = s.charAt(left);
-                windowFreq.put(d, windowFreq.get(d) - 1);
-                if (targetFreq.containsKey(d) && windowFreq.get(d).intValue() < targetFreq.get(d).intValue())
-                    formed--;
-                left++;
-            }
-            right++;
+        if (s.length() < t.length()) {
+            return "";
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char ch : t.toCharArray()) {
+            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+        }
+        int targetCharsRemaining = t.length();
+        int[] minWindow = {0, Integer.MAX_VALUE};
+        int startIndex = 0;
+        for (int endIndex = 0; endIndex < s.length(); endIndex++) {
+            char ch = s.charAt(endIndex);
+            if (charCount.containsKey(ch) && charCount.get(ch) > 0) {
+                targetCharsRemaining--;
+            }
+            charCount.put(ch, charCount.getOrDefault(ch, 0) - 1);
+            if (targetCharsRemaining == 0) {
+                while (true) {
+                    char charAtStart = s.charAt(startIndex);
+                    if (charCount.containsKey(charAtStart) && charCount.get(charAtStart) == 0) {
+                        break;
+                    }
+                    charCount.put(charAtStart, charCount.getOrDefault(charAtStart, 0) + 1);
+                    startIndex++;
+                }
+                if (endIndex - startIndex < minWindow[1] - minWindow[0]) {
+                    minWindow[0] = startIndex;
+                    minWindow[1] = endIndex;
+                }
+                charCount.put(s.charAt(startIndex), charCount.getOrDefault(s.charAt(startIndex), 0) + 1);
+                targetCharsRemaining++;
+                startIndex++;
+            }
+        }
+        return minWindow[1] >= s.length() ? "" : s.substring(minWindow[0], minWindow[1] + 1);        
     }
 }
