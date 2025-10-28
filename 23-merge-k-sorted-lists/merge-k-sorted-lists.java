ed737714ad5fb33298fd2536223635ce54cabb8c
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-10-28 17:33:31 +0530

    Time: 4 ms (76.43%) | Memory: 44.5 MB (65.74%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null || lists.length==0){
+            return null;
+        }
+        var pq=new PriorityQueue<ListNode>((a,b)->a.val-b.val);
+        for(ListNode ln:lists){
+            if(ln!=null){
+              pq.add(ln);
+            }
+        }
+        var dummy = new ListNode();
+        var curr=dummy;
+        while(!pq.isEmpty()){
+            var min =pq.poll();
+            curr.next=min;
+            curr=curr.next;
+            if(min.next!=null){
+                pq.add(min.next);
+            }
+        }
+        return dummy.next;
+    }
+}
