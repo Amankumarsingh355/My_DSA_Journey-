dd0da2f1f8f1a413c4fb947b726fde1050017ad1
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-11-04 00:10:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (8.26%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.findPeakElement(new int[]{1,2,3,1}));
+        System.out.println(s.findPeakElement(new int[]{1,2,1,3,5,6,4}));
+    }
+}
