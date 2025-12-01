5c71ec95c5fcf19a104b28f49444fe21813a8d03
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2025-12-01 19:34:49 +0530

    Time: 26 ms (24.73%) | Memory: 71.8 MB (52.20%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.java
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.java
@@ -1,15 +1,15 @@
-class Solution {
-    public long maxRunTime(int n, int[] batteries) {
+class Solution{
+    public long maxRunTime(int n, int[] batteries){
         long totalEnergy = 0;
-        for (int battery : batteries) {
+        for (int battery : batteries){
             totalEnergy += battery;
         }
         Arrays.sort(batteries);
-        for (int i = batteries.length - 1; i >= 0; i--) {
-            if (batteries[i] > totalEnergy / n) {
+        for (int i = batteries.length - 1; i >= 0; i--){
+            if (batteries[i] > totalEnergy / n){
                 totalEnergy -= batteries[i];
                 n--;
-            } else {
+            } else{
                 break;
             }
         }
