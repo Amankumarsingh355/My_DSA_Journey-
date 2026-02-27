fe90d450ad58fea1c19660bd0aeb8a78981813e1
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-02-27 06:05:54 +0530

    Time: 8 ms (70.97%) | Memory: 47.9 MB (74.19%) - LeetSync

--- 3983-minimum-operations-to-equalize-binary-string/minimum-operations-to-equalize-binary-string.java
+++ 3983-minimum-operations-to-equalize-binary-string/minimum-operations-to-equalize-binary-string.java
@@ -1,6 +1,5 @@
 class Solution {
     public int minOperations(String s, int k) {
-
         long  n = s.length(), zero = 0;
         
 
@@ -10,15 +9,12 @@ class Solution {
         }
         long one = n - zero;
         int ans =-1;
-
         if( zero == 0) return 0;
 
         for(long i =1 ; i <=n ;i++)
         {
             long flip = i * (long)k;
-
             if((flip - zero) % 2 != 0) continue;
-
             if(i % 2 == 1)
             {
                if(flip >= zero && flip <= (zero * i + one * (i - 1)))
