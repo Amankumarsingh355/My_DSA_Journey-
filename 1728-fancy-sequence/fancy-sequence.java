125cccfe0a0f19cd210a1f8fed732ae28a543e61
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-03-15 23:10:24 +0530

    Time: 44 ms (96.55%) | Memory: 124.7 MB (43.10%) - LeetSync

---
+++ 1728-fancy-sequence/fancy-sequence.java
@@ -0,0 +1,39 @@
+class Fancy {
+
+    long mod = 1000000007;
+    ArrayList<Long> val = new ArrayList<>();
+    long a = 1, b = 0;
+    long modPow(long x, long y){
+        long res = 1;
+        x %= mod;
+        while(y > 0){
+            if((y & 1) == 1){
+                res = (res * x) % mod;
+            }
+            x = (x * x) % mod;
+            y >>= 1;
+        }
+        return res;
+    }
+
+    public Fancy(){}
+
+    public void append(int v){
+        long x = (v - b + mod) % mod;
+        val.add((x * modPow(a, mod - 2)) % mod);
+    }
+
+    public void addAll(int inc){
+        b = (b + inc) % mod;
+    }
+
+    public void multAll(int m){
+        a = (a * m) % mod;
+        b = (b * m) % mod;
+    }
+
+    public int getIndex(int idx){
+        if(idx >= val.size()) return -1;
+        return (int)((a * val.get(idx) + b) % mod);
+    }
+}
