feeb502eb9a716995736d270fdcd1f89e29a29c4
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-03-25 03:19:44 +0530

    Time: 9 ms (90.24%) | Memory: 101.2 MB (31.22%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.java
+++ 3031-construct-product-matrix/construct-product-matrix.java
@@ -3,7 +3,9 @@ class Solution {
         final int MOD = 12345;
         int n = grid.length;
         int m = grid[0].length;
+
         int[][] ans = new int[n][m];
+
         long prefix = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -11,6 +13,7 @@ class Solution {
                 prefix = (prefix * grid[i][j]) % MOD;
             }
         }
+
         long suffix = 1;
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
