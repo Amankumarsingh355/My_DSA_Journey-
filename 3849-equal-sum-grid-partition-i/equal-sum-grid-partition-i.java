2e3c3451a1c0b6598deddc6fb4aaa96630a1cb5c
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-03-26 02:08:02 +0530

    Time: 24 ms (5.59%) | Memory: 181.8 MB (5.59%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -1,11 +1,8 @@
 class Solution {
     int n, m;
-
     boolean checkHz(int[][] grid){
         HashSet<Long> st = new HashSet<>();
-
         long pref = 0;
-
         for(int i = 0; i < n; i++){
             long rowSum = 0;
             for(int j = 0; j < m; j++){
@@ -14,17 +11,12 @@ class Solution {
             pref += rowSum;
             st.add(pref);
         }
-
         if(pref % 2 != 0) return false;
-
         return st.contains(pref / 2);
     }
-
     boolean checkVz(int[][] grid){
         HashSet<Long> st = new HashSet<>();
-
         long pref = 0;
-
         for(int j = 0; j < m; j++){
             long colSum = 0;
             for(int i = 0; i < n; i++){
@@ -33,16 +25,12 @@ class Solution {
             pref += colSum;
             st.add(pref);
         }
-
         if(pref % 2 != 0) return false;
-
         return st.contains(pref / 2);
     }
-
     public boolean canPartitionGrid(int[][] grid) {
         n = grid.length;
         m = grid[0].length;
-
         return checkHz(grid) || checkVz(grid);
     }
 }
