5d13e5608b750209c91dd6b45701b40a9e627756
Aman kumar singh <amankumarsinghrajput2020@gmail.com>
2026-03-26 13:01:55 +0530

    Time: 318 ms (52.94%) | Memory: 273.8 MB (82.35%) - LeetSync

--- 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.java
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.java
@@ -1,113 +1,209 @@
 class Solution {
-    int m;
-    int n;
     public boolean canPartitionGrid(int[][] grid) {
+        HashMap<Long, Integer> h = new HashMap<Long, Integer>();
         long totalSum = 0;
-        m = grid.length;
-        n = grid[0].length;
-        
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 totalSum += grid[i][j];
+                long t = grid[i][j] * 1l;
+                if (!h.containsKey(t)) {
+                    h.put(t, 1);
+                } else {
+                    h.put(t, h.get(t) + 1);
+                }
             }
         }
         long sum = 0;
-        HashSet<Long> potentialHalfValue = new HashSet<>();
-        HashMap<Long, List<int[]>> map = new HashMap<>();
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                long val = grid[i][j];
-                sum += val;
-                if (!map.containsKey(val)) {
-                    map.put(val, new ArrayList<>());
-                    if ((totalSum+val)%2 == 0)
-                        potentialHalfValue.add((totalSum+val)/2);
+        HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+        for (int i = 0; i < grid.length - 1; i++) {
+            {
+                for (int j = 0; j < grid[i].length; j++) {
+                    sum += grid[i][j];
+                    long t = grid[i][j] * 1l;
+                    if (!map.containsKey(t)) {
+                        map.put(t, 1);
+                    } else {
+                        map.put(t, map.get(t) + 1);
+                    }
                 }
-                map.get(val).add(new int[]{i,j}); 
+                if (sum == totalSum - sum) {
+                    return true;
+                } else {
+                    if (sum > (totalSum - sum)) {
+                        long p = sum - (totalSum - sum);
+                        if (i != 0) {
+                            if (map.containsKey(p)) {
+                                if(grid[0].length==1)
+                                {
+                                    long r=grid[0][0]*1l;
+                                    long s=grid[i][0]*1l;
+                                     if (r == p || s == p) {
+                                    return true;
+                                }
+                                    
+                                }
+                                else
+                                {
+                                return true;
+                                }
+                            }
+                        } else {
+                            long r = grid[0][0] * 1l;
+                            long s = grid[0][grid[0].length - 1] * 1l;
+                            if (grid[0].length != 1) {
+                                if (r == p || s == p) {
+                                    return true;
+                                }
+                            }
+                        }
+                    } else {
+                        long p = (totalSum - sum) - sum;
+                        if (i != grid.length - 2) {
+                            if (h.containsKey(p)) {
+                                int x = h.get(p);
+                                if (map.containsKey(p)) {
+                                    int y = map.get(p);
+                                    if (y < x) {
+                                        if(grid[0].length!=1)
+                                        {
+                                        return true;
+                                        }
+                                        else
+                                        {
+                                            long r=grid[grid.length-1][0]*1l;
+                                            long s=grid[i+1][0]*1l;
+                                            if(r==p || s==p)
+                                            {
+                                                return true;
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    if(grid[0].length!=1)
+                                        {
+                                        return true;
+                                        }
+                                        else
+                                        {
+                                            long r=grid[grid.length-1][0]*1l;
+                                            long s=grid[i+1][0]*1l;
+                                            if(r==p || s==p)
+                                            {
+                                                return true;
+                                            }
+                                        }
+                                }
+                            }
+                        } else {
+                            long r = grid[grid.length - 1][0] * 1l;
+                            long s = grid[grid.length - 1][grid[0].length - 1] * 1l;
+                            if (grid[0].length != 1) {
+                                if (r == p || s == p) {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+
             }
-            if (sum*2 == totalSum) return true;//no-removing Case
-            if (potentialHalfValue.contains(sum)){ //remove one cell
-                long removingValue = sum*2-totalSum;
-                if(connectCheck(map.get(removingValue), i, -1)) return true;
-            }
+
         }
+        map = new HashMap<Long, Integer>();
         sum = 0;
-        map.clear();
-        potentialHalfValue.clear();
-        for (int i = m-1; i >= 0; i--){
-            for (int j = 0; j < n; j++){
-                long val = grid[i][j];
-                sum += val;
-                if (!map.containsKey(val)) {
-                    map.put(val, new ArrayList<>());
-                    if ((totalSum+val)%2 == 0)
-                        potentialHalfValue.add((totalSum+val)/2);
+        for (int i = 0; i < grid[0].length - 1; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                long t = grid[j][i] * 1l;
+                if (!map.containsKey(t)) {
+                    map.put(t, 1);
+                } else {
+                    map.put(t, map.get(t) + 1);
                 }
-                map.get(val).add(new int[]{i,j}); 
+                sum += grid[j][i];
             }
-            if (sum*2 == totalSum) return true;//no-removing Case
-            if (potentialHalfValue.contains(sum)){ //remove one cell
-                long removingValue = sum*2-totalSum;
-                if(connectCheck(map.get(removingValue), i, -1)) return true;
-            }
-        }
-        sum = 0;
-        map.clear();
-        potentialHalfValue.clear();
-        for (int j = 0; j < n; j++){
-            for (int i = 0; i < m; i++){
-                long val = grid[i][j];
-                sum += val;
-                if (!map.containsKey(val)) {
-                    map.put(val, new ArrayList<>());
-                    if ((totalSum+val)%2 == 0)
-                        potentialHalfValue.add((totalSum+val)/2);
+            if (sum == (totalSum - sum)) {
+                return true;
+            } else {
+                if (sum > totalSum - sum) {
+                    if (i != 0) {
+                        long p = sum - (totalSum - sum);
+                        if (map.containsKey(p)) {
+                           if(grid.length>1)
+                           {
+                            return true;
+                           }
+                            else
+                           {
+                                long r = grid[0][0] * 1l;
+                                long s = grid[0][i] * 1l;
+                                 if (r == p || s == p) {
+                                return true;
+                            }
+                           }
+                        }
+                    } else {
+                        long p = sum - (totalSum - sum);
+                        long r = grid[0][0] * 1l;
+                        long s = grid[grid.length - 1][0] * 1l;
+                        if(grid.length!=1)
+                        {
+                            if(r==p || s==p)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                } else {
+                    long p = (totalSum - sum) - sum;
+                    if (i != grid[0].length - 2) {
+                        if (h.containsKey(p)) {
+                            int x = h.get(p);
+                            if (map.containsKey(p)) {
+                                int y = map.get(p);
+                                if (y < x) {
+                                    if(grid.length!=1)
+                                    {
+                                       return true;
+                                    }
+                                    else
+                                    {
+                                        long r=grid[0][i+1]*1l;
+                                        long s=grid[0][grid[0].length-1]*1l;
+                                        if(r==p || s==p)
+                                        {
+                                            return true;
+                                        }
+                                    }
+                                }
+                            } else {
+                                if(grid.length!=1)
+                                    {
+                                       return true;
+                                    }
+                                    else
+                                    {
+                                        long r=grid[0][i+1]*1l;
+                                        long s=grid[0][grid[0].length-1]*1l;
+                                        if(r==p || s==p)
+                                        {
+                                            return true;
+                                        }
+                                    }
+                            }
+                        }
+                    } else {
+                        long r = grid[0][grid[0].length - 1] * 1l;
+                        long s = grid[grid.length - 1][grid[0].length - 1] * 1l;
+                        if (grid.length != 1) {
+                            if (r == p || s == p) {
+                                return true;
+                            }
+                        }
+                    }
                 }
-                map.get(val).add(new int[]{i,j}); 
-            }
-            if (sum*2 == totalSum) return true;//no-removing Case
-            if (potentialHalfValue.contains(sum)){ //remove one cell
-                long removingValue = sum*2-totalSum;
-                if(connectCheck(map.get(removingValue), -1, j)) return true;
             }
         }
-        sum = 0;
-        map.clear();
-        potentialHalfValue.clear();
-        for (int j = n-1; j >= 0; j--){
-            for (int i = 0; i < m; i++){
-                long val = grid[i][j];
-                sum += val;
-                if (!map.containsKey(val)) {
-                    map.put(val, new ArrayList<>());
-                    if ((totalSum+val)%2 == 0)
-                        potentialHalfValue.add((totalSum+val)/2);
-                }
-                map.get(val).add(new int[]{i,j}); 
-            }
-            if (sum*2 == totalSum) return true;//no-removing Case
-            if (potentialHalfValue.contains(sum)){ //remove one cell
-                long removingValue = sum*2-totalSum;
-                if(connectCheck(map.get(removingValue), -1, j)) return true;
-            }
-        }
-        return false;
-    }
-    private boolean connectCheck(List<int[]> positions, int row, int col){
-        for (int[] posi : positions){
-            boolean valid = true;
-            if (row == 0 || row == m-1){ //one part is single row, we can only remove from two end
-                if (posi[1]!=0 && posi[1]!=n-1) valid = false;
-            }else if (col == 0 || col == n-1){ //one part is single column, we can only remove from two end
-                if (posi[0]!=0 && posi[0]!=m-1) valid = false;
-            }
-            if (row > 0 && n == 1){ //for single column matrix, only three cell are safe to remove
-                if (posi[0] != 0 && posi[0] != m-1 && posi[0] != row ) valid = false;
-            }else if (col > 0 && m == 1){ //for single row matrix, only three cell are safe to remove
-                if (posi[1] != 0 && posi[1] != n-1 && posi[1] != col) valid = false;
-            }
-            if (valid) return true;
-        }
+
         return false;
     }
 }
